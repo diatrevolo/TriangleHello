940c7e0cf842d80488b5a1ef2ca8f21ff8aed8dd
Roberto J Osorio <roberto@av-controls.com>
2018-11-27 10:10:15 -0500

    Initial Commit

---
+++ TriangleHello/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TriangleHello
+//
+//  Created by Roberto on 11/27/18.
+//  Copyright © 2018 AV&C. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ TriangleHelloTests/TriangleHelloTests.swift
@@ -0,0 +1,34 @@
+//
+//  TriangleHelloTests.swift
+//  TriangleHelloTests
+//
+//  Created by Roberto on 11/27/18.
+//  Copyright © 2018 AV&C. All rights reserved.
+//
+
+import XCTest
+@testable import TriangleHello
+
+class TriangleHelloTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
